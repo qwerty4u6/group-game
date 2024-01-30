111b3a6d239874abdbf546f6ac0ff6102e3be6bd
qwerty4u6 <catgdthrowaway@gmail.com>
2024-01-30 14:30:31 -0800

    vclass ending not writing this

--- scenes/battle.gd
+++ scenes/battle.gd
@@ -14,7 +14,7 @@ static var team_members = [
 		"max_mana": 10,
 	},
 	{
-		"name": "blablablablablab",
+		"name": "blahblah",
 		"level": 4,
 		"max_hp": 16,
 		"max_mana": 10,
@@ -26,5 +26,20 @@ var char_stat_displays = []
 @onready var char_stat_display_scene = preload("res://scenes/char_stat_display.tscn")
 
 func _ready():
-	var new_disp = char_stat_display_scene.instantiate()
-	add_child(new_disp)
+	var i = 0
+	for member in team_members:
+		var disp = char_stat_display_scene.instantiate()
+		char_stat_displays.push_back(disp)
+		add_child(disp)
+		
+		disp.get_node("NameLabel").text = member.name
+		disp.get_node("LevelLabel").text = "lvl. " + str(member.level)
+		var hp = member.max_hp
+		var hp_bar = disp.get_node("HealthBar")
+		hp_bar.max_value = hp
+		hp_bar.value = hp
+		hp_bar.get_node("Label").text = str(hp)
+		#disp.get_node("ManaBar/Label").text = str(member.max_mana)
+		disp.position.y = i * 120
+		i += 1
+	
---
+++ scenes/char_stat_display.gd
@@ -0,0 +1,10 @@
+extends Panel
+
+var hp_bar
+var hp_label
+
+func _ready():
+	hp_bar = $HealthBar
+
+func change_hp(hp):
+	pass
