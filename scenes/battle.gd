cc5c53c472ac5bd4e437c418485b568861d3f663
qwerty4u6 <catgdthrowaway@gmail.com>
2024-02-01 14:30:51 -0800

    idfk

--- scenes/battle.gd
+++ scenes/battle.gd
@@ -1,52 +1,27 @@
 extends Control
 
-static var team_members = [
-	{
-		"name": "plhldr 1",
-		"level": 8,
-		
-		"max_hp": 18,
-		"hp": 18,
-		"max_mana": 12,
-		"mana": 12,
-	},
-	{
-		"name": "plhldr 2",
-		"level": 5,
-		
-		"max_hp": 15,
-		"hp": 15,
-		"max_mana": 10,
-		"mana": 10,
-	},
-	{
-		"name": "blahblah",
-		"level": 4,
-		
-		"max_hp": 16,
-		"hp": 16,
-		"max_mana": 10,
-		"mana": 10,
-	}
-]
-
-var char_stat_displays = []
+var team_resources
+var members = []
 
 @onready var char_stat_display_scene = preload("res://scenes/char_stat_display.tscn")
 
 func _ready():
+	team_resources = [
+		load("res://resources/player characters/ninja.tres"),
+		load("res://resources/player characters/ninja2.tres"),
+		load("res://resources/player characters/ninja3.tres")
+	]
 	var i = 0
-	var offset = (806 - team_members.size() * 120) / 2
-	for member in team_members:
+	var offset = (806 - team_resources.size() * 120) / 2
+	for member in team_resources.size():
 		var disp = char_stat_display_scene.instantiate()
-		char_stat_displays.push_back(disp)
+		members.push_back(disp)
 		add_child(disp)
+		disp.init(team_resources[i])
 		
-		disp.get_node("NameLabel").text = member.name
-		disp.get_node("LevelLabel").text = "lvl. " + str(member.level)
-		disp.set_max_hp(member.max_hp)
-		disp.set_hp(member.hp)
-		disp.set_max_mana(member.max_mana)
-		disp.set_mana(member.mana)
 		disp.position = Vector2(6, offset + i * 120)
 		i += 1
+
+func _input(event):
+	if event.is_action_pressed("move_up"):
+		pass
--- scenes/char_stat_display.gd
+++ scenes/char_stat_display.gd
@@ -1,5 +1,7 @@
 extends Panel
 
+var res
+
 var hp_bar
 var hp_label
 var mana_bar
@@ -10,6 +12,16 @@ func _ready():
 	hp_label = hp_bar.get_node("Label")
 	mana_bar = $ManaBar
 	mana_label = mana_bar.get_node("Label")
+
+func init(member):
+	res = member
+	get_node("NameLabel").text = res.name
+	get_node("LevelLabel").text = "lvl. " + str(res.level)
+	get_node("TextureRect").texture = res.texture
+	set_max_hp(res.max_hp)
+	set_hp(res.hp)
+	set_max_mana(res.max_mana)
+	set_mana(res.mana)
 
 func set_max_hp(hp):
 	hp_bar.max_value = hp
