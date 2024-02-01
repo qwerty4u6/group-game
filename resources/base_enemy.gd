fa977ceb07cb4e53152a1c4958af192f1883f188
qwerty4u6 <catgdthrowaway@gmail.com>
2024-02-01 12:34:48 -0800

    started working on resource system

---
+++ resources/base_enemy.gd
@@ -0,0 +1,7 @@
+extends Resource
+
+@export var name: String = "Enemy"
+@export var texture: Texture = null
+@export var level: int = 10
+@export var max_hp: int = 25
+@export var hp: int = 25
---
+++ resources/base_player.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+@export var name: String = "Player"
+@export var texture: Texture = null
+@export var level: int = 10
+@export var max_hp: int = 15
+@export var hp: int = 15
+@export var max_mana: int = 25
+@export var mana: int = 25
+
