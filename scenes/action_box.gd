04fa7fe99da4482232609eeb5a1f4a4e841ad13a
qwerty4u6 <catgdthrowaway@gmail.com>
2024-02-26 12:49:26 -0800

    worked more on Butto ns

--- scenes/action_box.gd
+++ scenes/action_box.gd
@@ -3,6 +3,7 @@ extends Panel
 @onready var battle = get_parent()
 @onready var text_box = get_node("MarginContainer/TextBox")
 @onready var action_box = get_node("MarginContainer/ActionBox")
+@onready var back_box = get_node("MarginContainer/BackBox")
 @onready var empty_box = get_node("MarginContainer/EmptyBox")
 @onready var text_box_label = text_box.get_node("MarginContainer/Label")
 
@@ -15,35 +16,47 @@ func _input(event):
 			text_box.hide()
 			action_box.show()
 			emit_signal("textbox_closed")
+			text_box_label.visible_characters = 0
 
 func to_textbox():
 	text_box.show()
 	action_box.hide()
+	back_box.hide()
 	empty_box.hide()
 
 func to_action_box(character = null):
 	text_box.hide()
 	action_box.show()
+	back_box.hide()
 	empty_box.hide()
 	
 	if character != null:
 		action_box.get_node("MarginContainer/MainContainer/MiscContainer/NameLabel").text = character.name_text
 		action_box.get_node("MarginContainer/MainContainer/MiscContainer/TextureRect").texture = character.texture
 
+func to_back_box():
+	text_box.hide()
+	action_box.hide()
+	back_box.show()
+	empty_box.hide()
+
 func to_empty_box():
 	text_box.hide()
 	action_box.hide()
+	back_box.hide()
 	empty_box.show()
 
 func show_text(text):
 	to_textbox()
 	text_box.get_node("NextLabel").hide()
 	
+	text_box_label.get_node("Timer").set_paused(false)
 	text_box_label.visible_characters = 0
 	text_box_label.get_node("Timer").start()
 	text_box_label.text = text
 	
 	await text_done_displaying
+	text_box_label.get_node("Timer").set_paused(true)
 	text_box.get_node("NextLabel").show()
 	$AnimationPlayer.play("next arrow")
 
@@ -52,5 +65,21 @@ func _on_timer_timeout():
 	if text_box_label.visible_characters == text_box_label.text.length() + 1:
 		emit_signal("text_done_displaying")
 
-func _on__pressed():
-	print(battle)
+func _on_skill_1_pressed():
+	do_skill(1)
+
+func _on_skill_2_pressed():
+	do_skill(2)
+
+func _on_skill_3_pressed():
+	do_skill(3)
+
+func _on_skill_4_pressed():
+	do_skill(4)
+
+func _on_back_pressed():
+	to_action_box()
+
+func do_skill(i):
+	to_back_box()
+	print(i)
