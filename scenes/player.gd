928529af28f086d0825db5c43a8fe020f0429180
windylandscape <catastrophe64music@gmail.com>
2024-04-13 21:36:19 -0700

    test

--- scenes/player.gd
+++ scenes/player.gd
@@ -17,7 +17,8 @@ func _process(delta):
 		velocity.x -= 1
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-	position += velocity.normalized() * 5
+	position += velocity.normalized() * 300 * delta
+	
 	if velocity.x < 0:
 		anim.flip_h = true
 	if velocity.x > 0:
